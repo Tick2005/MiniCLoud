0c4a7dd7ccf77618d0ba31c052f743c1ef01435b
PhanVanDuong <phanvanduong1223456@gmail.com>
2026-03-21 00:28:03 +0700

    update advance server 1,2,3 and successfull all the request in final project

--- relational-database-server/init/001_init.sql
+++ relational-database-server/init/001_init.sql
@@ -6,3 +6,20 @@ CREATE TABLE IF NOT EXISTS notes(
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 INSERT INTO notes(title) VALUES ('Hello from MariaDB!');
+
+CREATE DATABASE IF NOT EXISTS studentdb;
+USE studentdb;
+
+CREATE TABLE IF NOT EXISTS students(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    student_id VARCHAR(10) NOT NULL UNIQUE,
+    fullname VARCHAR(100) NOT NULL,
+    dob DATE NOT NULL,
+    major VARCHAR(50) NOT NULL
+);
+
+INSERT INTO students(student_id, fullname, dob, major)
+VALUES
+('SV001', 'Nguyen Van A', '2004-01-15', 'Cloud Computing'),
+('SV002', 'Tran Thi B', '2004-05-11', 'Software Engineering'),
+('SV003', 'Le Van C', '2003-12-22', 'Information Security');
